1ab34682b3cf9c2de99fc5b00ebc3c8ba11fce13
chakrabortyj <joey.chakraborty@gmail.com>
2018-03-20 23:35:14 -0400

    nodes update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,107 @@
 var systemMap = d3.select('#vis')
 
-var perLevel = [1, 5, 6, 4]
+var jsonNodes = [
+   {"level": 1,
+   	"order": 1,
+	"label": "Landing Page"
+   },
+   {"level": 2,
+   	"order": 1,
+	"label": "New Releases"
+   },
+   {"level": 2,
+   	"order": 2,
+	"label": "My Library"
+   },
+   {"level": 2,
+   	"order": 3,
+	"label": "Social"
+   },
+   {"level": 2,
+   	"order": 4,
+	"label": "Settings"
+   },
+   {"level": 2,
+   	"order": 5,
+	"label": "Sign Out"
+   },
+   {"level": 3,
+   	"order": 1,
+	"label": "Music"
+   },
+   {"level": 3,
+   	"order": 2,
+	"label": "Podcasts and Shows"
+   },
+   {"level": 3,
+   	"order": 3,
+	"label": "My Music"
+   },
+   {"level": 3,
+   	"order": 4,
+	"label": "My Podcasts"
+   },
+   {"level": 3,
+   	"order": 5,
+	"label": "Friends' Library"
+   },
+   {"level": 3,
+   	"order": 6,
+	"label": "Account"
+   },
+   {"level": 4,
+   	"order": 1,
+	"label": "Genres"
+   },
+   {"level": 4,
+   	"order": 2,
+	"label": "Today's Hits"
+   },
+   {"level": 4,
+   	"order": 3,
+	"label": "Profile"
+   },
+   {"level": 4,
+   	"order": 4,
+	"label": "Subscription"
+   }
+
+];
 
 var svgSelection = systemMap.append('svg')
-	.attr('width', 400)
-	.attr('height', 400);
+	.attr('width', 600)
+	.attr('height', 600);
+
+
+/*d3.json("nodes.json", function(error, data){
+
+	data.forEach(function(d){
+		d.level = d.level;
+		d.order = d.order;
+		d.title = d.title;
+	});
+
+	
+});*/
+
 
 var nodes = svgSelection.selectAll("circle")
-	.data(perLevel)
+	.data(jsonNodes)
 	.enter()
-	.append("circle")
+	.append("circle");
 
 var nodeAttributes = nodes
-	.attr("cx", (function (d) {return 25*d+1;}))
-	.attr("cy", (function (d) {return 25*d+1;}))
-	.attr("r", 25)
-	.attr("class", "state-default");
+	.attr("cx", (function (d) {return 300+25*(d.level);}))
+	.attr("cy", (function (d) {return 150+25*(d.level+2*d.order);}))
+	.attr("r", 50)
+	.attr("class", "state-default")
+	.append("text")
+	.attr("dx", function(d){return 10})	
+	.attr("fill", "red")
+	.text(function(d){return d.label});
+
+
+
+
 
 
